7f464a18bf80aa45dc1573d22da191794d7b0db4
CHEGEBB <chegephil24@gmail.com>
2024-03-28 12:47:48 +0300

    js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-const form = querySelector('#newsleter-form');
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    console.log(e);
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.querySelector('.card1 form');
+    const card1 = document.querySelector('.card1');
+    const card2 = document.querySelector('.card2');
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        card1.classList.add('hide');
+        card2.classList.remove('hide');
+    });
 });
