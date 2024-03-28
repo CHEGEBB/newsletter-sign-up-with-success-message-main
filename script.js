03b001f0f4306206188006dafbc46fe10bec909f
CHEGEBB <chegephil24@gmail.com>
2024-03-28 13:02:51 +0300

    update

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const form = document.querySelector('.card1 form');
+    const form = document.querySelector('.inputs form');
     const card1 = document.querySelector('.card1');
     const card2 = document.querySelector('.card2');
+    const dismissButton = document.querySelector('.card2 button[type="dismiss"]');
 
-    form.addEventListener('submit', (e) => {
+    form.addEventListener('submit', function(e) {
         e.preventDefault();
-        card1.classList.add('hide');
-        card2.classList.remove('hide');
+        const emailInput = document.querySelector('#email').value;
+        if (emailInput.trim() !== '') {
+            card1.classList.add('hide');
+            card2.classList.remove('hide');
+        }
+    });
+
+    dismissButton.addEventListener('click', function() {
+        card2.classList.add('hide');
+        card1.classList.remove('hide');
     });
 });
